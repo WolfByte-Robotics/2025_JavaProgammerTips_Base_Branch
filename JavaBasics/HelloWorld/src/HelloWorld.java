cf9ea6124b4703065d35bb204b1417b38e4a730b
doeflow <dorian.flowers@ignatius.org>
2025-06-13 16:59:15 -0500

    Create Simple Hello Worfld Java Application
    Simple Hello World Java Program

---
+++ JavaBasics/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/***************************************************
+ * 
+ *  Author: Mr. Flowers
+ *  Description: Simple Hello World Java Program
+ *  Date: 6.13.2025
+ * 
+ ***************************************************/
+
+public class HelloWorld {
+
+	/* Within main, the application uses System.out.println method to 
+	 * to display "Hello World" to a console window
+	 */
+	public static void main(String [] args)
+	{
+		System.out.println("Hello World");
+	}
+	
+}
