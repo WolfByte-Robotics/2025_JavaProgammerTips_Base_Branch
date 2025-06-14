d9acc8df224a3e596efadbc04c68cfd056174feb
doeflow <dorian.flowers@ignatius.org>
2025-06-13 19:42:03 -0500

    Create Project for Understanding Semicolons and Renamed Folders to Reference Difficulty Levels
    Create Project for Understanding Semicolons and Renamed Folders to Reference Difficulty Levels

---
+++ JavaBasics/0002_UnderstandingSemicolon/src/UnderstandingSemicolon.java
@@ -0,0 +1,23 @@
+/******************************************************
+ * 
+ *  Author: Mr. Flowers
+ *  Description: Program to Understand Semicolon Usage
+ *  Date: 6.13.2025
+ * 
+ ******************************************************/
+public class UnderstandingSemicolon {
+	public static void main(String [] args)
+	{
+		// use semicolon to separate one instruction from the next
+		int int_width = 10;
+		int int_length = 20;
+		
+		// Java does not care that your statements appear on
+		// individual lines. You can write the same set of 
+		// instructions on one line as seen below.
+		int width = 10; int length = 20;  
+		
+		//NOTE: Because the java compiler ignore comments, no need for semicolon after this comment
+		
+	}
+}
