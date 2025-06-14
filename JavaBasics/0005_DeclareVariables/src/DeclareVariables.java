e5552a6a3191621de51eba01ac10fd8af24c064e
doeflow <dorian.flowers@ignatius.org>
2025-06-14 10:46:13 -0500

    Program Showing How to Declare Variables

---
+++ JavaBasics/0005_DeclareVariables/src/DeclareVariables.java
@@ -0,0 +1,33 @@
+ /*******************************************************
+  *  
+  *  Author: Mr. Flowers
+  *  Description: Program Showing How to Declare Variables
+  *  Date: 6.14.2025
+  * 
+  ******************************************************/
+public class DeclareVariables {
+	
+	public static void main(String [] args)
+	{
+		/* When declaring a variable within your program, you must
+		 * tell the Java compiler the variable's name and type.
+		 * The following statements declare three integers and
+		 * two boolean type variables
+		 */
+		
+		int age;
+		int weight;
+		int height;
+		boolean smokes;
+		boolean exercises;
+		
+		
+		// When declare variables of the same type, Java lets you
+		// list and declare variables on one or more lines
+		// separated by commas
+		int intAge, intWeight, intHeight;
+		boolean bSmokes, bExercises;
+		
+		
+	}
+}
