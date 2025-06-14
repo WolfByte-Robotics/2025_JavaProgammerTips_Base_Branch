4b79d2206831489472a9854a09f424375a308ebc
doeflow <dorian.flowers@ignatius.org>
2025-06-14 10:12:11 -0500

    Program to Show Code Comment Usage

---
+++ JavaBasics/0004_UsingComments/src/UsingComments.java
@@ -0,0 +1,32 @@
+ /*******************************************************
+  *  
+  *  Author: Mr. Flowers
+  *  Description: Program to Show Code Comment Usage
+  *  Date: 6.14.2025
+  * 
+  ******************************************************/
+public class UsingComments {
+	
+	public static void main(String [] args)
+	{
+		
+		/*
+		 This comment covers several lines in the program file. The Java
+		 compiler will completely ignore the contents within this line.
+		 Use this style of comments when you need to use several lines in 
+		 the file for documentation.
+		 */
+		
+		// You may use this style commenting to comment out a single line of code
+		//int value = 29;
+		
+		// You can use comments to document the action of specific line of code
+		String strValue = "Hello World";   // Set strValue to String value "Hello World"
+		System.out.println(strValue);
+		
+		/* You can also use block commenting to comment a block of executable code
+		 // int number = 10;  // Set number to Integer value 10 
+		 */
+	}
+
+}
