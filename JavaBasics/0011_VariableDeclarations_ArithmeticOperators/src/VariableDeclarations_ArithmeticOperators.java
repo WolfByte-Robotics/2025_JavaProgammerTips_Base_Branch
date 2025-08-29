7f0f0b402c70429f1059369f0cd63166f732a511
doeflow <dorian.flowers@ignatius.org>
2025-08-28 22:00:28 -0500

    Moved files around and Implemented Code for Arithmetic Operations in Variable Declarations

---
+++ JavaBasics/0011_VariableDeclarations_ArithmeticOperators/src/VariableDeclarations_ArithmeticOperators.java
@@ -0,0 +1,19 @@
+ /*******************************************************
+  *  
+  *  Author: Mr. Flowers
+  *  Description: How to Cast Primitive Types
+  *  Date: 8.28.2025
+  * 
+  ******************************************************/
+public class VariableDeclarations_ArithmeticOperators {
+
+	public static void main (String [] args)
+	{
+		// When you declare variables in Java, you can assign
+		// initial values to the variables. As it turns out
+		// you may use an arithmetic expression as an assignment
+		float circumference = 2f * 3.14f * 3.4f;
+		
+		System.out.println("The circumference is " + circumference);	
+	}
+}
