af911522651302d92818ece1e727f3692dda0c1d
doeflow <dorian.flowers@ignatius.org>
2025-08-28 20:12:29 -0500

    How to Display Primitive Data Types

---
+++ 0008_DisplayingPrimativeTypes/src/DisplayingPrimitiveDataTypes.java
@@ -0,0 +1,22 @@
+ /*******************************************************
+  *  
+  *  Author: Mr. Flowers
+  *  Description: How to Display Primitive Types
+  *  Date: 8.28.2025
+  * 
+  ******************************************************/
+public class DisplayingPrimitiveDataTypes {
+	public static void main(String [] args)
+	{
+		// As you develop and later debug code, you will
+		// find it necessary to display values of variables
+		// at different locations in your program
+		short s = 15431;
+		int i = 123456;
+		float f = 3.14159f;
+		
+		// With Concatenation (+), you can automatically 
+		// convert and append its operands to strings
+		System.out.println("short: " + s + " int: " + i + " float: " + f);
+	}	
+}
